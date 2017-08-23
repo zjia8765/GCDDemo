498360456e945d6594f915471e43884a8def75e7
zhangjia <zhangjia107483@pwrd.com>
2017-08-23 12:15:17 +0800

    增加httpDNS demo

--- GCDDemo/ViewController.m
+++ GCDDemo/ViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
-#define KDouBanURL  @"https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=%E5%8C%97%E4%BA%AC&start=0&count=100&client=somemessage&udid=dddddddddddddddddddddd"
+#define KDouBanURL  @"https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=%E5%8C%97%E4%BA%AC&start=0&count=10"
 
 #define KWeatherURL @"https://weatherapi.market.xiaomi.com/wtr-v3/weather/all?latitude=110&longitude=112&isLocated=true&locationKey=weathercn%3A101010100&days=15&appKey=weather20151024&sign=zUFJoAR2ZVrDy1vF3D07&romVersion=7.2.16&appVersion=87&alpha=false&isGlobal=false&device=cancro&modDevice=&locale=zh_cn"
 
-@interface ViewController ()
-
+@interface ViewController ()<NSURLSessionTaskDelegate>
+@property (nonatomic, strong) NSMutableURLRequest *myRequest;
 @end
 
 @implementation ViewController
@@ -216,4 +217,178 @@
         dispatch_semaphore_signal(semaphore);
     });
 }
+
+
+
+
+#pragma mark httpDNS NSURLSession
+- (IBAction)httpDNSRqeusetURLSession:(id)sender {
+    NSURL *originalUrl =[NSURL URLWithString:KDouBanURL];
+    NSURL *url = [self getIpByHostAsync:originalUrl];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    //将request的host字段改为原始URL的域名
+    [request setValue:originalUrl.host forHTTPHeaderField:@"host"];
+    request.HTTPMethod = @"GET";
+    self.myRequest = request;
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+                                                completionHandler:^(NSData *_Nullable data, NSURLResponse *_Nullable response, NSError *_Nullable error) {
+                                                    NSLog(@"%@",response);
+                                                }];
+    [dataTask resume];
+    
+}
+
+- (BOOL)evaluateServerTrust:(SecTrustRef)serverTrust
+                  forDomain:(NSString *)domain {
+    /*
+     * 创建证书校验策略
+     */
+    NSMutableArray *policies = [NSMutableArray array];
+    if (domain) {
+        [policies addObject:(__bridge_transfer id) SecPolicyCreateSSL(true, (__bridge CFStringRef) domain)];
+    } else {
+        [policies addObject:(__bridge_transfer id) SecPolicyCreateBasicX509()];
+    }
+    /*
+     * 绑定校验策略到服务端的证书上
+     */
+    SecTrustSetPolicies(serverTrust, (__bridge CFArrayRef) policies);
+    /*
+     * 评估当前serverTrust是否可信任，
+     * 官方建议在result = kSecTrustResultUnspecified 或 kSecTrustResultProceed
+     * 的情况下serverTrust可以被验证通过，https://developer.apple.com/library/ios/technotes/tn2232/_index.html
+     * 关于SecTrustResultType的详细信息请参考SecTrust.h
+     */
+    SecTrustResultType result;
+    SecTrustEvaluate(serverTrust, &result);
+    return (result == kSecTrustResultUnspecified || result == kSecTrustResultProceed);
+}
+
+#pragma mark - NSURLSessionTaskDelegate
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential *_Nullable))completionHandler {
+    if (!challenge) {
+        return;
+    }
+    NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+    NSURLCredential *credential = nil;
+    /*
+     * 获取原始域名信息。
+     */
+    NSString *host = [[self.myRequest allHTTPHeaderFields] objectForKey:@"host"];
+    if (!host) {
+        host = self.myRequest.URL.host;
+    }
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+        if ([self evaluateServerTrust:challenge.protectionSpace.serverTrust forDomain:host]) {
+            disposition = NSURLSessionAuthChallengeUseCredential;
+            credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+        } else {
+            disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        }
+    } else {
+        disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+    }
+    // 对于其他的challenges直接使用默认的验证方案
+    completionHandler(disposition, credential);
+}
+
+#pragma mark httpDNS AFNetworking
+- (IBAction)httpDNSRqeusetAF:(id)sender {
+    NSURL *originalUrl =[NSURL URLWithString:KDouBanURL];
+    NSURL *url = [self getIpByHostAsync:originalUrl];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    //将request的host字段改为原始URL的域名
+    [request setValue:originalUrl.host forHTTPHeaderField:@"host"];
+    request.HTTPMethod = @"GET";
+
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] init];
+     NSURLSessionDataTask *task = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+         NSLog(@"%@",response);
+     }];
+
+    [manager setSessionDidReceiveAuthenticationChallengeBlock:^NSURLSessionAuthChallengeDisposition(NSURLSession * _Nonnull session, NSURLAuthenticationChallenge * _Nonnull challenge, NSURLCredential *__autoreleasing  _Nullable * _Nullable credential) {
+        //跳过证书校验过程
+        NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+            *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            disposition = NSURLSessionAuthChallengeUseCredential;
+        } else {
+            disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        }
+        return disposition;
+    }];
+    
+    [manager setTaskDidReceiveAuthenticationChallengeBlock:^NSURLSessionAuthChallengeDisposition(NSURLSession * _Nonnull session, NSURLSessionTask * _Nonnull task, NSURLAuthenticationChallenge * _Nonnull challenge, NSURLCredential *__autoreleasing  _Nullable * _Nullable credential) {
+        //跳过证书校验过程
+        NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+            *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            disposition = NSURLSessionAuthChallengeUseCredential;
+        } else {
+            disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        }
+        return disposition;
+    }];
+    
+    [task resume];
+}
+
+- (IBAction)httpDNSRqeusetAFPost:(id)sender {
+    
+    NSString *weatherUrl = @"https://api.douban.com/v2/movie/in_theaters";
+    NSURL *originalUrl =[NSURL URLWithString:weatherUrl];
+    NSURL *url = [self getIpByHostAsync:originalUrl];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    //将request的host字段改为原始URL的域名
+    [request setValue:originalUrl.host forHTTPHeaderField:@"host"];
+    request.HTTPMethod = @"POST";
+    
+    NSString *postString = @"apikey=0b2bdeda43b5688921839c8ecb20399b&city=北京&start=0&count=10";
+    [request setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] init];
+    NSURLSessionDataTask *task = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+        NSLog(@"%@",response);
+    }];
+    
+    [manager setSessionDidReceiveAuthenticationChallengeBlock:^NSURLSessionAuthChallengeDisposition(NSURLSession * _Nonnull session, NSURLAuthenticationChallenge * _Nonnull challenge, NSURLCredential *__autoreleasing  _Nullable * _Nullable credential) {
+        //跳过证书校验过程
+        NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+            *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            disposition = NSURLSessionAuthChallengeUseCredential;
+        } else {
+            disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        }
+        return disposition;
+    }];
+    
+    [manager setTaskDidReceiveAuthenticationChallengeBlock:^NSURLSessionAuthChallengeDisposition(NSURLSession * _Nonnull session, NSURLSessionTask * _Nonnull task, NSURLAuthenticationChallenge * _Nonnull challenge, NSURLCredential *__autoreleasing  _Nullable * _Nullable credential) {
+        //跳过证书校验过程
+        NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+            *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            disposition = NSURLSessionAuthChallengeUseCredential;
+        } else {
+            disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        }
+        return disposition;
+    }];
+    
+    [task resume];
+}
+
+- (NSURL *)getIpByHostAsync:(NSURL *)url {
+    NSString *host = url.host;
+    NSString *ipUrlStr = [url.absoluteString stringByReplacingOccurrencesOfString:host withString:@"115.182.201.6"];
+    NSURL *ipURL = [NSURL URLWithString:ipUrlStr];
+    return ipURL;
+    
+    
+}
+
 @end
